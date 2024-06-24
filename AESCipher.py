8b38f97322c8ca1215ee2a6792124873c13bce11
GalTenne <116973464+GalTenne@users.noreply.github.com>
2024-06-24 17:37:44 +0300

    Update AESCipher.py for the older version

--- AESCipher.py
+++ AESCipher.py
@@ -2,28 +2,21 @@ import base64
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
-import hmac
 
 class AESCipher(object):
 
     def __init__(self, key):
         self.bs = AES.block_size
-        self.key = hashlib.sha512(key.encode()).digest()
+        self.key = hashlib.sha256(key.encode()).digest()
 
     def encrypt(self, raw):
         # generate the unique IV
         iv = Random.new().read(AES.block_size)
 
-        # slice the 512bit key
-        kh = self.key[:32]
-        km = self.key[32:]
-
-        # pad data + AES
+        # pad data
         raw = self._pad(raw)
-        c = AES.new(kh, AES.MODE_CBC, iv).encrypt(raw.encode())
-
-        m = hmac.new(km, c.encode(), hashlib.sha512).digest()
-        return base64.b64encode(iv + c + m)
+        c = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + c.encrypt(raw.encode()))
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
